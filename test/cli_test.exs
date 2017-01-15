2b8a4ca6e4fe72859086bb41ef1d7be9be89dcb9
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2017-01-15 16:14:31 -0500

    Fix test

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -8,8 +8,4 @@ defmodule CliTest do
     assert parse_args(["-h", "anything"]) == :help
     assert parse_args(["--help", "anything"]) == :help
   end
-
-  test "three values returned if three given" do
-    assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
-  end
 end
