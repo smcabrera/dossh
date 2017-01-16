7f1842634008560dbff1f2118df62e11cbb27cc1
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2017-01-16 16:02:01 -0500

    WIP: so far able to make table its own command, still working on arguments for ls

--- lib/dossh/cli.ex
+++ lib/dossh/cli.ex
@@ -11,38 +11,66 @@ defmodule Dossh.CLI do
   end
 
   @doc """
-  `argv` can be -h or --help, which returns :help.
-   if no arguments are given it will simply return the table of names and ip addresses
+  `argv` can be
+    -h or --help, which returns :help.
+    table, which will return a table of names and ip addresses of the user's digital ocean droplets
   """
 
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [ help: :boolean ],
+    parse = OptionParser.parse(argv, switches: [ "help": :boolean, "ip-address": :boolean, "name": :boolean],
                                      aliases:  [ h:    :help ])
     case parse do
 
-      { [ help: true ], _, _ }
+      { [ help: true ] }
         -> :help
 
-      _ -> :ip_address
+      { [], ["table"], [] } 
+        -> :table
+
+      { [], ["ls"], [_] } 
+        -> :ls
 
     end
   end
 
   def process(:help) do
     IO.puts """
-      usage: dossh 
-      lists digital ocean droplets and ip addresses using a token stored in a dotfile in the user's home directory ~/.digital_ocean_token
+      usage: dossh table
+      outputs a table of digital ocean droplets with their names and ip addresses. Uses a token stored in a dotfile in the user's home directory: ~/.digital_ocean_token
+
+      dossh ls [-id]
+      lists ip addresses or droplet names. Defaults to both if no options specified. Separated by an equals sign
+
+      examples: 
+      $ dossh ls -i
+      192.34.785
+      184.62.849
+
+      $ dossh ls -d
+      ubuntu-64-1
+      ubuntu-64-2
+
+      $ dossh ls
+      ubuntu-64-1=192.34.785
+      ubuntu-64-2=184.62.849
     """
     System.halt(0)
   end
 
-  def process(:ip_address) do
+  def process(:table) do
     Dossh.Droplets.fetch
     |> render_table
   end
 
-  def droplet_info_string(droplet) do
-    " #{droplet[:droplet_name]}, ip address: #{droplet[:ip_address]}"
+  def process(:ls) do
+    Dossh.Droplets.fetch
+    |> render_list 
+  end
+
+  def render_list(droplets) do
+    Enum.map(droplets, fn(droplet) -> "#{droplet[:droplet_name]}=#{droplet[:ip_address]}" end)
+    |> Enum.join("\n")
+    |> IO.puts
   end
 
   def render_table(droplets) do
